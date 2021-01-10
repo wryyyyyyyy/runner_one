433e047f0c72411d629cbc950fe019af3b437928
wryyyyyyyy <no@spam.here>
2021-01-10 03:58:07 +0100

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo '#### build  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/runner_one.svg)' >Readme.md
 echo '#### status ![TravisCI](https://travis-ci.com/wryyyyyyyy/runner_one.svg)' >>README.md
-echo '###### checked' >>README.md
+echo '#### log prepared' >./docs/_site/log.txt
 sudo wget https://api.travis-ci.com/v3/job/470113424/log.txt >./docs/_site/log.txt
